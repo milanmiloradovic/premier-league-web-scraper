2203db42e589216550afb522eca9dcbcad7e57f4
Milan Miloradovic <milan.miloradovic98@gmail.com>
2022-09-18 19:28:29 +0200

    Initial commit

---
+++ src/main/java/premierleague/web/scraper/PremierLeagueWebScraperApplication.java
@@ -0,0 +1,13 @@
+package premierleague.web.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueWebScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueWebScraperApplication.class, args);
+	}
+
+}
